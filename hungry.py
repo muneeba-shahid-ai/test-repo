eee69439631d5b14fa718fda6521fce5df5b2683
muneeba-shahid-ai <munibashahid571@gmail.com>
2025-06-23 13:44:02 +0500

    homework

--- hungry.py
+++ hungry.py
@@ -2,5 +2,6 @@ hungry = input("am I hungry?")
 if hungry == 'yes':
     print('eat more')
     print("eat something healthy")
+    print("than do your homework")
 else:
     print("do your homework")    
