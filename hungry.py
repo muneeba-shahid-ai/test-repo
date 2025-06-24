cbbafae66e5d437fa565942665bc155a66a1c282
muneeba-shahid-ai <munibashahid571@gmail.com>
2025-06-24 14:02:23 +0500

    take rest

--- hungry.py
+++ hungry.py
@@ -3,5 +3,6 @@ if hungry == 'yes':
     print('eat more')
     print("eat something healthy")
     print("than do your homework")
+    print("take rest as well")
 else:
     print("do your homework")    
