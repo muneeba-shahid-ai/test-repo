07e93fb8306984d82cb2deb3abca4154d7d06f80
muneeba shahid <munibashahid571@gmail.com>
2025-06-24 15:46:49 +0500

    Update hungry.py

--- hungry.py
+++ hungry.py
@@ -1,5 +1,5 @@
 hungry = input("am I hungry?")
-if hungry == 'yes':
+if hungry == 'yes'and hungry == "ofcourse":
     print('eat more')
     print("eat something healthy")
     print("than do your homework")
